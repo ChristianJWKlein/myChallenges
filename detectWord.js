e8005ebc3f8b7b7958bbbe517a6eb11107a86133
Christian John William Klein <cjwk@Christians-MacBook-Air.local>
2022-01-24 22:56:51 -0500

    My Code Challenges

---
+++ detectWord.js
@@ -0,0 +1,31 @@
+// A word is on the loose and now has tried to hide amongst a crowd of small letters!
+// Help write a function to detect what the word is, knowing the following rules:
+// The wanted word is in uppercase.
+// Note that the word will be spread out amongst the random letters, but their letters remain in the same order.
+// Examples
+// detectWord(word1) ➞ “BOCA CODE”
+// detectWord(word2) ➞ “HELLO”
+
+//Multiple ways to do this one.
+//
+
+const word1 = "aaHeEaLLaaaO";
+const word2 = "BxxOxxCxxAx xCxxOxxxDxxE";
+
+function strProccesor(str) {
+  let newWord = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == str[i].toUpperCase()) {
+      //Says if string is true if uppper case letter present. Loop to specify by index to test characters.
+      //console.log(str[i]);                 //Alt. method
+      //newWord = newWord + str[i];          //method
+      newWord.push(str[i]);
+    }
+  }
+  let final = newWord.join(""); // .join("") value in the commas will be added between each array value.
+  console.log(final);
+}
+
+strProccesor(word1);
+strProccesor(word2);
---
+++ printMultTable.js
@@ -0,0 +1,38 @@
+/*
+1 2 3 4 5 6 7 8 9 10
+2 4 6 8 10 12 14 16 18 20
+3 6 9 12 15 18 21 24 27 30
+4 8 12 16 20 24 28 32 36 40
+5 10 15 20 25 30 35 40 45 50
+6 12 18 24 30 36 42 48 54 60
+7 14 21 28 35 42 49 56 63 70
+8 16 24 32 40 48 56 64 72 80
+9 18 27 36 45 54 63 72 81 90
+10 20 30 40 50 60 70 80 90 100
+*/
+
+//The below nested for loop creates and 2-D array of desired values above.
+
+for (let i = 1; i <= 10; i++) {
+  let newArray = []; //makes array empty for every iteration of i.  So first value of array is 10th iteration of i, first of j
+  for (let j = 1; j <= 10; j++) {
+    newArray.push(j * i);
+  }
+  console.log(...newArray); //takes values of the array and diplayes them
+}
+
+//The below version accepts an array of any size and outputs in the same manor.
+
+let ranArray = [1, 3, 5, 7, 9];
+
+function nestIteration(arr) {
+  for (let i = 1; i <= arr.length; i++) {
+    let newArray = [];
+    for (let j = 1; j <= arr.length; j++) {
+      newArray.push(j * i);
+    }
+    console.log(...newArray);
+  }
+}
+
+nestIteration(ranArray);

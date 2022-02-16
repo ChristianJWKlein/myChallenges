6fbe0e801fb17ef5fe7a6367c040f03c7b31917a
Christian JW Klein <cklein010715@gmail.com>
2022-02-16 17:39:08 -0500

    added in alt. method

--- recursion.js
+++ recursion.js
@@ -3,6 +3,17 @@
 //For recursion base case is needed so that it stops.
 //Fibonachi.. 0, 1,1,2,3,5,8,13,21, 34,55
 
+// function countdown(n) {
+//   if (n == 0) {
+//     //basecase
+//     console.log(0);
+//   } else {
+//     console.log(n);
+//     countdown(n - 1);
+//   }
+// }
+// countdown(10);
+
 function countdown(n) {
   if (n < 2) {
     //basecase

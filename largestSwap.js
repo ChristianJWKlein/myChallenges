f1db61c00d0e31bda76f9a1638000bf75b2109a6
Christian JW Klein <cklein010715@gmail.com>
2022-03-26 21:31:28 -0400

    largestSwap

---
+++ largestSwap.js
@@ -0,0 +1,17 @@
+// Write a function that takes a two-digit number and determines if it's the largest of two possible digit swaps.
+
+// largestSwap(27) ➞ false
+
+// largestSwap(43) ➞ true
+
+const largestSwap = (twoDigitNum) => {
+  let reverseString = twoDigitNum.toString().split('').reverse().join('');
+  let reverseNumber = parseInt(reverseString);
+  if (reverseNumber > twoDigitNum) {
+    return false;
+  } else if (reverseNumber < twoDigitNum) {
+    return true;
+  }
+};
+
+console.log(largestSwap(36));

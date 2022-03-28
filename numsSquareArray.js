94af7ec75c9c10201af67d632214da4a90e0f32a
Christian JW Klein <cklein010715@gmail.com>
2022-03-28 11:53:14 -0400

    alt methods

--- numsSquareArray.js
+++ numsSquareArray.js
@@ -13,3 +13,26 @@ console.log(...numsSquaredArr);
 for (val of number) {
   console.log(val ** 2);
 }
+
+//give me the sum of the numbers in an array.  Give me a function.
+
+const arrayOfNumbers = [4, 7, 9, 2, 5];
+
+function addArrayElements(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+console.log(addArrayElements(arrayOfNumbers));
+
+const addArrElements = (arr) => {
+  let sum = 0;
+  for (let val of arr) {
+    sum += val;
+  }
+  return console.log(sum);
+};
+
+addArrElements(arrayOfNumbers);

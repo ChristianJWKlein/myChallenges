698d6c74eb955d36f3131b2a22908ab4bb24e2c9
Christian JW Klein <cklein010715@gmail.com>
2022-04-01 21:58:10 -0400

    hours to seconds

---
+++ hoursToSeconds.js
@@ -0,0 +1,9 @@
+// Write a function that converts hours into seconds.
+const hours = 6
+
+function hoursToSeconds(numHours) {
+  let seconds = numHours * 60 * 60
+  return seconds
+}
+
+console.log(hoursToSeconds(hours))

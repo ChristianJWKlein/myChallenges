8ba9fb8772e3b7eea23db5a90022e43027e266f7
Christian JW Klein <cklein010715@gmail.com>
2022-05-04 23:56:20 -0400

    todo

---
+++ olderThanMe.js
@@ -0,0 +1,18 @@
+// Create a method in the Person class which returns how another person's age compares. Given the instances p1, p2 and p3, which will be initialised with the attributes name and age, return a sentence in the following format:
+
+//{other person name} is {older than / younger than / the same age as} me.
+
+// p1 = Person('Thomas', 24)
+
+// p2 = Person('Imperial', 36)
+
+let persona = {
+  compareAge: () => {
+    console.log('tom')
+  },
+}
+
+// p1.compareAge(p2) ➞ "Joel is older than me."
+// Person.compareAge()
+// console.log(Person)
+persona.compareAge()

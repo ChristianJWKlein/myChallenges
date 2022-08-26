e55d9ffa3e6ec8722c3cd6cda8879cf90358ccd8
Christian JW Klein <cklein010715@gmail.com>
2022-08-26 00:41:31 -0400

    different approach

---
+++ addTwoNums.js
@@ -0,0 +1,29 @@
+//function that adss two numbers
+
+const a = 5
+const b = 7
+
+const addTwoNums = (num1, num2) => {
+  let answer = num1 + num2
+  return answer
+}
+
+console.log(addTwoNums(a, b))
+
+//function that adds the numbers of an array
+
+const numArr = [44, 6, 7, 8, 88, 23, 5, 98, 23]
+
+const addNumArr = (arr) => {
+  let accumulator = 0
+  for (let val of arr) {
+    let watcher = val
+    for (let otherVal of arr) {
+      answer === answer + watcher
+    }
+  }
+}
+
+//return
+
+console.log(addNumArr(numArr))

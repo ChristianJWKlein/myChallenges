cc2f3a6f86184d79a22d9c3649106646f9c5f477
Christian JW Klein <cklein010715@gmail.com>
2022-09-05 11:45:16 -0400

    1st way

--- perimetersWithACatch.js
+++ perimetersWithACatch.js
@@ -1,4 +1,4 @@
-// Write a function that takes a number and returns the perimeter of either a circle or a square. The input will be in the form (letter l, number num) where the letter will be either "s" for square, or "c" for circle, and the number will be the side of the square or the radius of the circle.
+// Write a function that takes a number and returns the perimeter of either a circle or a square. The input will be in the form (letter l, number num) where the letter will be either "s" for square, or "c" for circle, and the number will be the side of the square or the radius of the circle. No Rounding is needed.
 
 // Perimeter of a square: 4 * side.
 // Perimeter of a circle: 6.28 * radius.
@@ -15,6 +15,17 @@
 
 // perimeter("s", 7) ➞ 28
 
-// perimeter("c", 4) ➞ 25.12
+const perimeter = (letter, num) => {
+  const lett = letter
+  if (lett === 's') {
+    console.log(4 * num)
+  } else if (lett === 'c') {
+    console.log(6.28 * num)
+  } else {
+    console.log('invalid input.  Enter "s" or "c", along with the number')
+  }
+}
+
+perimeter('r', 4)
 
 // perimeter("c", 9) ➞ 56.52

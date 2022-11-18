39400dd4448cae28e2d79ced0c78e50ebc605a0d
Christian JW Klein <cklein010715@gmail.com>
2022-11-17 22:18:22 -0500

    Process data types

---
+++ burglarySeries-4-addName.js
@@ -0,0 +1,18 @@
+// Given three arguments ⁠— an object obj of the stolen items, the pets name and a value ⁠— return an object with that name and value in it (as key-value pairs).
+
+// addName({}, "Brutus", 300) ➞ { Brutus: 300 }
+
+// addName({ piano: 500 }, "Brutus", 400) ➞ { piano: 500, Brutus: 400 }
+
+// addName({ piano: 500, stereo: 300 }, "Caligula", 440) ➞ { piano: 500, stereo: 300, Caligula: 440 }
+
+//Return everything in an object
+//Handle and return 1st object KVP.  Return KVP for each item in first object. IF empty return nothing
+//Append string and value as KVP
+
+const addName = (obj, str, num) => {
+  let returnObj = obj
+  return Object.assign(obj, { [str]: num })
+}
+
+console.log(addName({ piano: 500 }, 'Brutus', 400))

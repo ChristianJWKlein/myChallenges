c24dfaa101100e3b5067a4160dfdd2787000f86e
Christian JW Klein <cklein010715@gmail.com>
2022-11-29 23:39:31 -0500

    function factory

--- functionFactory.js
+++ functionFactory.js
@@ -5,19 +5,25 @@
 // Calling makePlusFunction(5) returns a new function that takes an input,
 // and returns the result when adding 5 to it.
 
+const makePlusFunction = (num1) => {
+  return function (num2) {
+    return num2 + num1
+  }
+}
+
 const plusFive = makePlusFunction(5)
 
-//plusFive(2) ➞ 7
+console.log(plusFive(2)) //➞ 7
 
-//plusFive(-8) ➞ -3
+console.log(plusFive(-8)) // ➞ -3
 
 // Calling makePlusFunction(10) returns a new function that takes an input,
 // and returns the result when adding 10 to it.
 
 const plusTen = makePlusFunction(10)
 
-//plusTen(0) ➞ 10
+console.log(plusTen(0)) // ➞ 10
 
-//plusTen(188) ➞ 198
+console.log(plusTen(188)) // ➞ 198
 
-//plusFive(plusTen(0)) ➞ 15
+console.log(plusFive(plusTen(0))) // ➞ 15

1fb8797933f8a5ae3eabb58fe6ff3c2ae5ac67da
Christian JW Klein <cklein010715@gmail.com>
2022-12-01 23:26:01 -0500

    num digits in num

---
+++ numDigsInNum.js
@@ -0,0 +1,14 @@
+// Create a function that will return an integer number corresponding to the amount of digits in the given integer num.
+
+const num_of_digits = (num) => {
+  let numString = num.toString()
+  console.log(numString.length)
+}
+
+num_of_digits(1000) //➞ 4
+
+num_of_digits(12) //➞ 2
+
+num_of_digits(1305981031) //➞ 10
+
+num_of_digits(0) //➞ 1

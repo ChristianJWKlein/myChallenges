a6f42b90ef8f7c582ffde3fd8fe1b7408735fa57
Christian JW Klein <cklein010715@gmail.com>
2025-01-30 23:51:59 -0500

    partial solution

--- listUpArrayPropper.js
+++ listUpArrayPropper.js
@@ -1,5 +1,25 @@
 // Given an array of strings (nouns), list them up in a complete sentence.
 
+const sentence = (arr) => {
+  //   let finalSentence = ''
+  let finalSentencePieces = []
+  for (let i = 0; i <= arr.length - 1; i++) {
+    let firstLetter = arr[i][0]
+    if (
+      firstLetter.includes('a') ||
+      firstLetter.includes('e') ||
+      firstLetter.includes('i') ||
+      firstLetter.includes('o') ||
+      firstLetter.includes('u')
+    ) {
+      finalSentencePieces.push(`An ${arr[i]}`)
+    } else {
+      finalSentencePieces.push(`A ${arr[i]}`)
+    }
+  }
+  console.log(finalSentencePieces)
+}
+
 // Examples
 sentence(['orange', 'apple', 'pear']) //➞ "An orange, an apple and a pear."
 
@@ -15,3 +35,10 @@ sentence(['car', 'plane', 'truck', 'boat']) //➞ "A car, a plane, a truck and a
 // The sentence ends with a .
 // There are at least two nouns given.
 // Every given word is lowercase.
+
+//For every value in the array, if the first letter of the string is a vowel (a ,e ,i ,o and u), we write An... else A
+//We then log the first value
+//Place a comma
+//Repeat for every value in the array until the last value
+// At the last value we log "and a/an" instead of comma, then log the value with a .
+// Wrap it up in quotes

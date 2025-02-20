748556d88153d88e617ced00589a2f2d01db2bd4
Christian JW Klein <cklein010715@gmail.com>
2025-02-19 22:44:08 -0500

    review

--- shuffledProperly.js
+++ shuffledProperly.js
@@ -40,7 +40,7 @@ isShuffledWell([1, 2, 3, 5, 8, 6, 9, 10, 7, 4]) //➞ false
 //isShuffledWell([3, 5, 1, 9, 8, 7, 6, 4, 2, 10]) //➞ false
 // 9, 8, 7, 6 appear consecutively
 
-isShuffledWell([1, 5, 3, 8, 10, 2, 7, 6, 4, 9]) //➞ true
+//isShuffledWell([1, 5, 3, 8, 10, 2, 7, 6, 4, 9]) //➞ true
 // No consecutive numbers appear
 
 //isShuffledWell([1, 3, 5, 7, 9, 2, 4, 6, 8, 10]) //➞ true
@@ -49,3 +49,5 @@ isShuffledWell([1, 5, 3, 8, 10, 2, 7, 6, 4, 9]) //➞ true
 // Notes
 // Only steps of 1 in either direction count as consecutive (i.e. a sequence of odd and even numbers would count as being properly shuffled (see example #4)).
 // You will get numbers from 1-10.
+
+// need to figure out how to only log false or true once for each array
